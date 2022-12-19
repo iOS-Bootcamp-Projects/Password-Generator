87f5bae273dc21d3b75a28c6df533207f15c083a
aamer Essa <aamer.es12@gmail.com>
2022-12-19 12:06:42 +0300

    Initial Commit

---
+++ Password Generator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Password Generator
+//
+//  Created by Aamer Essa on 19/12/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
